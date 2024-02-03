71c67b61156bfc33ca18fd51b66cf67617bc0a77
Daniel Rocha <rochadaniel@acad.ifma.edu.br>
2024-02-03 16:42:35 -0300

    Some fixes

--- jpm.sh
+++ jpm.sh
@@ -9,10 +9,11 @@ DEPENDENCIES_FILE=$WORKDIR/$DEPENDENCIES
 SOURCE_PATTERN=src/main/java/
 SOURCE=$WORKDIR/$SOURCE_PATTERN
 REMOTE_REPO=https://raw.githubusercontent.com/danroxha/jpm/main/jpm.sh
+LOG_SUFFIX=[JPM]
 
 log() {
   d_format=$(date +"%Y-%m-%d %H:%M:%S")
-  echo $d_format $1
+  echo $d_format $LOG_SUFFIX $1
 }
 
 jpm_clean() {
@@ -77,18 +78,18 @@ jpm_resolve() {
       if [ ! -e $JPM_MODULE/$dependency ]
         then
           qtd_dep=$(( qtd_dep + 1 ))
-          log "dependency's resolving	 $dependency [$url]"
+          log "dependency	[loading]  $dependency [$url]"
           wget $url -P $JPM_MODULE -q ?> /dev/null &
         else
-      log "dependency resolved $dependency"
+          log "dependency [loaded] $dependency"
       fi
     done    
   _await $qtd_dep
 
-  log "local .jpm "$JPM_MODULE
+  log "path "$JPM_MODULE
 }
 
-jpm_load_dependecies() {
+jpm_load_dependencies() {
   libs=$BUILD_DIR/
   for _jar in $(ls $BUILD_LIB) 
     do
@@ -100,19 +101,21 @@ jpm_load_dependecies() {
 
 jpm_install() {
   mkdir -p $BUILD_DIR $BUILD_LIB
-  cp -r $JPM_MODULE* $BUILD_LIB
-
-  if [[ ! -z $(ls $JPM_MODULE) ]]
+ 
+  if [ -d $JPM_MODULE ] && [[ ! -z $(ls $JPM_MODULE) ]]
     then
-      dependecies=$(jpm_load_dependecies)
+      cp -r $JPM_MODULE* $BUILD_LIB
+      dependencies=$(jpm_load_dependencies)
+    else
+      log "didn't find any dependencies, you might want to run \`$ jpm resolve\`"
     fi
     
   java_files=$(find $SOURCE -name "*.java")
   log "building $java_files"
   
-  if [ ! -z $dependecies ]
+  if [ ! -z $dependencies ]
     then
-      javac -d $BUILD_DIR $java_files -cp $dependecies
+      javac -d $BUILD_DIR $java_files -cp $dependencies
     else
       javac -d $BUILD_DIR $java_files
     fi
@@ -133,7 +136,7 @@ jpm_find_main_file() {
     done
    if [ $_qtd -gt 1 ]
      then
-       log "Cannot defined main class [ $main_file ]"
+       log "cannot defined main class [ $main_file ]"
        exit 1
      fi
    echo $main_file	
@@ -158,10 +161,10 @@ jpm_run() {
   jpm_resolve
   jpm_install
     
-  dependecies=$(jpm_load_dependecies)
+  dependencies=$(jpm_load_dependencies)
   main_class=$(jpm_find_main_ref)
   
-  java -cp $dependecies $main_class
+  java -cp $dependencies $main_class
 }
 
 jpm_package() {
@@ -204,41 +207,229 @@ _capitalize() {
   echo $_cap
 }
 
-jpm_new() {
-  suffix=Application
-  echo "New project JPM"
-  
-  echo -ne "project name: "
-  read project_name
-  project_name=$(_lowercase $project_name)
-  
-  echo -ne "package name: "
-  read package_name
-  package_name=$(_lowercase $package_name)
+_create_directories() {
+  _project_dir=$1
+  _package_dir=$(echo $2 | sed 's/\./\//g')
+  _path=$WORKDIR/$_project_dir/$SOURCE_PATTERN$_package_dir
 
-  echo -ne "main class: "
-  read main_class
+  mkdir -p $_path
+  log "created "$_path
+}
 
-  _pkg=$package_name
-  package_name=$(echo $package_name | sed 's/\./\//g')
+_create_main_class_file() {
+  _suffix=Application
+  _project_dir=$1
+  _pkg=$2
+  _main_class=$3
+  _package_dir=$(echo $_pkg | sed 's/\./\//g')
+  _path=$_project_dir/$SOURCE_PATTERN$_package_dir
+  _classname=$_main_class$_suffix.java
 
-  mkdir -p $project_name/$SOURCE_PATTERN/$package_name
+  log "created  $WORKDIR/$_path/$_classname"
 
-  cat << EOF > $project_name/$SOURCE_PATTERN/$package_name/$main_class$suffix.java
+  cat << EOF > $_path/$_classname
 package $_pkg;
 
-public class $main_class$suffix {
+public class $_main_class$_suffix {
   public static void main(String[] args) {
     System.out.println("Hello JPM");
   }
 }
 EOF
+}
 
-  log "Donwloading JPM"
+_create_gitignore() {
+  _project_dir=$1
+  cat << EOF > $_project_dir/.gitignore
+# JPM
 
-  wget $REMOTE_REPO -P $project_name -q ?> /dev/null
-  chmod +x $project_name/jpm.sh
+.jpm/*
+target/*
 
+### Java ###
+# Compiled class file
+*.class
+
+# Log file
+*.log
+
+# BlueJ files
+*.ctxt
+
+# Mobile Tools for Java (J2ME)
+.mtj.tmp/
+
+# Package Files #
+*.jar
+*.war
+*.nar
+*.ear
+*.zip
+*.tar.gz
+*.rar
+
+# virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
+hs_err_pid*
+replay_pid*
+
+### VisualStudioCode ###
+.vscode/*
+!.vscode/settings.json
+!.vscode/tasks.json
+!.vscode/launch.json
+!.vscode/extensions.json
+!.vscode/*.code-snippets
+
+# Local History for Visual Studio Code
+.history/
+
+# Built Visual Studio Code Extensions
+*.vsix
+
+### VisualStudioCode Patch ###
+# Ignore all local history of files
+.history
+.ionide
+
+# Created by https://www.toptal.com/developers/gitignore/api/intellij
+# Edit at https://www.toptal.com/developers/gitignore?templates=intellij
+
+### Intellij ###
+# Covers JetBrains IDEs: IntelliJ, RubyMine, PhpStorm, AppCode, PyCharm, CLion, Android Studio, WebStorm and Rider
+# Reference: https://intellij-support.jetbrains.com/hc/en-us/articles/206544839
+
+# User-specific stuff
+.idea/**/workspace.xml
+.idea/**/tasks.xml
+.idea/**/usage.statistics.xml
+.idea/**/dictionaries
+.idea/**/shelf
+
+# AWS User-specific
+.idea/**/aws.xml
+
+# Generated files
+.idea/**/contentModel.xml
+
+# Sensitive or high-churn files
+.idea/**/dataSources/
+.idea/**/dataSources.ids
+.idea/**/dataSources.local.xml
+.idea/**/sqlDataSources.xml
+.idea/**/dynamic.xml
+.idea/**/uiDesigner.xml
+.idea/**/dbnavigator.xml
+
+# Gradle
+.idea/**/gradle.xml
+.idea/**/libraries
+
+# Gradle and Maven with auto-import
+# When using Gradle or Maven with auto-import, you should exclude module files,
+# since they will be recreated, and may cause churn.  Uncomment if using
+# auto-import.
+# .idea/artifacts
+# .idea/compiler.xml
+# .idea/jarRepositories.xml
+# .idea/modules.xml
+# .idea/*.iml
+# .idea/modules
+# *.iml
+# *.ipr
+
+# CMake
+cmake-build-*/
+
+# Mongo Explorer plugin
+.idea/**/mongoSettings.xml
+
+# File-based project format
+*.iws
+
+# IntelliJ
+out/
+
+# mpeltonen/sbt-idea plugin
+.idea_modules/
+
+# JIRA plugin
+atlassian-ide-plugin.xml
+
+# Cursive Clojure plugin
+.idea/replstate.xml
+
+# SonarLint plugin
+.idea/sonarlint/
+
+# Crashlytics plugin (for Android Studio and IntelliJ)
+com_crashlytics_export_strings.xml
+crashlytics.properties
+crashlytics-build.properties
+fabric.properties
+
+# Editor-based Rest Client
+.idea/httpRequests
+
+# Android studio 3.1+ serialized cache file
+.idea/caches/build_file_checksums.ser
+
+### Intellij Patch ###
+# Comment Reason: https://github.com/joeblau/gitignore.io/issues/186#issuecomment-215987721
+
+# *.iml
+# modules.xml
+# .idea/misc.xml
+# *.ipr
+
+# Sonarlint plugin
+# https://plugins.jetbrains.com/plugin/7973-sonarlint
+.idea/**/sonarlint/
+
+# SonarQube Plugin
+# https://plugins.jetbrains.com/plugin/7238-sonarqube-community-plugin
+.idea/**/sonarIssues.xml
+
+# Markdown Navigator plugin
+# https://plugins.jetbrains.com/plugin/7896-markdown-navigator-enhanced
+.idea/**/markdown-navigator.xml
+.idea/**/markdown-navigator-enh.xml
+.idea/**/markdown-navigator/
+
+# Cache file creation bug
+# See https://youtrack.jetbrains.com/issue/JBR-2257
+.idea/$CACHE_FILE$
+
+# CodeStream plugin
+# https://plugins.jetbrains.com/plugin/12206-codestream
+.idea/codestream.xml
+
+# Azure Toolkit for IntelliJ plugin
+# https://plugins.jetbrains.com/plugin/8053-azure-toolkit-for-intellij
+.idea/**/azureSettings.xml
+
+EOF
+}
+
+_create_dependencies_file() {
+  _project_dir=$1
+  cat << EOF > $_project_dir/$DEPENDENCIES
+# Support to Gradle (Short). Only Central Repository
+# implementation 'aws.sdk.kotlin:aws-core-jvm:1.0.48'
+# 
+# // https://mvnrepository.com/artifact/org.keycloak/keycloak-core
+# implementation 'org.keycloak:keycloak-core:23.0.5'
+
+// https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
+implementation 'org.xerial:sqlite-jdbc:3.45.1.0'
+
+// https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+implementation 'org.slf4j:slf4j-api:1.7.36'
+
+EOF
+}
+
+
+_show_welcome() {
   echo "
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ░░░░░░   ░░        ░░░   ░░░░░░░   ░
@@ -273,22 +464,34 @@ $ jpm run
 
 Para mais informações: https://github/danroxha/jpm"
   
-  cat << EOF > $project_name/$DEPENDENCIES
-# Support to Gradle (Short). Only Central Repository
-# implementation 'aws.sdk.kotlin:aws-core-jvm:1.0.48'
-# 
-# // https://mvnrepository.com/artifact/org.keycloak/keycloak-core
-# implementation 'org.keycloak:keycloak-core:23.0.5'
+}
 
-// https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
-implementation 'org.xerial:sqlite-jdbc:3.45.1.0'
+jpm_new() {
+  echo "New project JPM"
+  
+  echo -ne "$LOG_SUFFIX project name: "
+  read project_name
+  project_name=$(_lowercase $project_name)
+  
+  echo -ne "$LOG_SUFFIX package name: "
+  read package_name
+  package_name=$(_lowercase $package_name)
 
-// https://mvnrepository.com/artifact/org.slf4j/slf4j-api
-implementation 'org.slf4j:slf4j-api:1.7.36'
+  echo -ne "$LOG_SUFFIX main class: "
+  read main_class
 
-EOF
+  log "building project $project_name"
 
-  echo "local "$WORKDIR/$project_name
+  _create_directories $project_name $package_name
+  _create_main_class_file $project_name $package_name $main_class
+  _create_dependencies_file $project_name
+  _create_gitignore $project_name
+
+  wget -q $REMOTE_REPO -P $project_name ?> /dev/null
+  log "donwloaded JPM"
+  chmod +x $project_name/jpm.sh
+
+  log "path: $WORKDIR/$project_name"
 }
 
 for arg in $* 
